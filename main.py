3208d79b75b730a570e83b59a99c85fc31f65343
Justin Williamson <justin5192@gmail.com>
2023-03-08 19:52:49 -0500

    Added decode() function

--- main.py
+++ main.py
@@ -4,22 +4,33 @@ def encode(password):
     encoded_password = ''
     for i in password:
         if int(i) <= 6:
-            encoded_password = str(int(i) + 3)
+            encoded_password += str(int(i) + 3)
         else:
-            encoded_password = str(int(i) - 7)
+            encoded_password += str(int(i) - 7)
     return encoded_password
+
+def decode(password):
+    converted_string = ''
+    for i in password:
+        if int(i) >= 3:
+            converted_string += str(int(i) - 3)
+        else:
+            converted_string += str(int(i) + 7)
+
+    return converted_string
 
 
 def main():
-    menu_on = True
-    while menu_on:
-        encoded_pw = ''
-        original_pw = ''
+    menu_option = True
+    encoded_pw = ''
+    original_pw = ''
+    while menu_option:
+
         print("Password Menu: ")
         print("1. Encode Password")
         print("2. Decode Password")
         print("3. Quit")
-        menu_option = input("Select a menu option: ")
+        menu_option = int(input("Select a menu option: "))
         if menu_option == 1:
             password = input("Enter password to encode: ")
             encoded_pw = encode(password)
@@ -29,8 +40,8 @@ def main():
             print(f'The encoded password is {encoded_pw}, and the original password is {original_pw}')
         if menu_option == 3:
             exit()
-        else:
-            print("Invalid menu option.")
+        #else:
+        #   print("Invalid menu option.")
 
 
 if __name__ == '__main__':
